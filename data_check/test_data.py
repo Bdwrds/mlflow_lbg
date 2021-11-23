1a0fd2b550a4c85d025feeb7300519fbc32ac8b8
Bdwrds <edwards.benjamin.james@gmail.com>
2021-11-23 16:46:08 +0000

    Completing basic tests

--- data_check/test_data.py
+++ data_check/test_data.py
@@ -5,14 +5,23 @@ date: 2021-11-22
 """
 import pandas as pd
 
+def get_variables_numeric(variables_yaml):
+    return variables_yaml['valid_numeric_variables']
+
+def get_variables_categorical(variables_yaml):
+    return list(variables_yaml['valid_categorical_variables'])
+
+def get_target(variables_yaml):
+    return list(variables_yaml['target'])
+
 def test_column_names(data, variables_yaml):
     """
     Check all required columns defined in yaml exist in data file
     """
-    features_numeric = variables_yaml['valid_numeric_features']
-    features_categorical = list(variables_yaml['valid_categorical_features'])
-    target = list(variables_yaml['target'])
-    all_required_columns = features_numeric + features_categorical + target
+    variables_numeric = get_variables_numeric(variables_yaml)
+    variables_categorical = get_variables_categorical(variables_yaml)
+    target = get_target(variables_yaml)
+    all_required_columns = variables_numeric + variables_categorical + target
     filtered_cols = data.columns[data.columns.isin(all_required_columns)].values
     assert set(filtered_cols) == set(all_required_columns)
     return filtered_cols
@@ -23,7 +32,26 @@ def test_row_count(data):
     """
     assert 15000 < data.shape[0] < 1000000
 
+def test_variable_numeric(data, variables_yaml):
+    """
+    Check all the numeric variables are numeric
+    """
+    variables_numeric = get_variables_numeric(variables_yaml)
+    for variable in variables_numeric:
+        assert pd.api.types.is_numeric_dtype(data.loc[:,variable].dtypes)
+
+def test_variable_category(data, variables_yaml):
+    """
+    Check that none of the categorical variables are numeric
+    """
+    variables_categorical = get_variables_categorical(variables_yaml)
+    for variable in variables_categorical:
+        assert pd.api.types.is_numeric_dtype(data.loc[:, variable].dtypes) != True
+
 def test_saving_down_file(data, data_output_name, variables_yaml):
+    """
+    Save down the dataset with all features and targets
+    """
     all_required_columns = test_column_names(data, variables_yaml)
     data_output = data.loc[:, all_required_columns]
     data_output.to_csv(data_output_name, index=False)
